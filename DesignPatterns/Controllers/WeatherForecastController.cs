dc5a9c100fe307e2cab4601674557835c9065fb3
Taras Ostapjuk <t0976487630@gmail.com>
2025-03-30 21:18:32 +0300

    Initial commit

--- DesignPatterns/Controllers/WeatherForecastController.cs
+++ DesignPatterns/Controllers/WeatherForecastController.cs
@@ -4,30 +4,19 @@ namespace DesignPatterns.Controllers
 {
   [ApiController]
   [Route( "[controller]" )]
-  public class WeatherForecastController : ControllerBase
+  public class CreationalPatternsController : ControllerBase
   {
-    private static readonly string[] Summaries = new[]
-    {
-          "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-      };
+    private readonly ILogger<CreationalPatternsController> _logger;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController( ILogger<WeatherForecastController> logger )
+    public CreationalPatternsController( ILogger<CreationalPatternsController> logger )
     {
       _logger = logger;
     }
 
     [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<bool> Get()
     {
-      return Enumerable.Range( 1, 5 ).Select( index => new WeatherForecast
-      {
-        Date = DateOnly.FromDateTime( DateTime.Now.AddDays( index ) ),
-        TemperatureC = Random.Shared.Next( -20, 55 ),
-        Summary = Summaries[Random.Shared.Next( Summaries.Length )]
-      } )
-      .ToArray();
+      return new List<bool>{ true, false };
     }
   }
 }
